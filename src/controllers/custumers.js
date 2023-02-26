66c5090cf8514ebf7abe1f008a905fa62512db10
Daniel Vigano <danielvigano17@gmail.com>
2023-02-26 20:48:12 -0300

    Adicionando criptografia de senhas

--- src/controllers/custumers.js
+++ src/controllers/custumers.js
@@ -1,18 +1,21 @@
 const CustumersModel = require('../models/custumers')
+const {crypto} = require('../utils/password')
 
-function add(req,res) {
+async function add(req,res) {
     const {
         name,
         age,
         email,
         password,
     } = req.body
+
+    const passwordCrypto = await crypto(password)
    
     const register = new CustumersModel({
         name,
         age,
         email,
-        password,
+        password: passwordCrypto,
     })
     register.save()
     res.send('cadastro realizado')
