08b5254f743ad63fa708670a597db0ad9c945668
Daniel Vigano <danielvigano17@gmail.com>
2023-02-26 19:40:52 -0300

    Estrutura final do sistema de cadastro

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,15 @@
 const routes = require('express').Router()
 
+const CustumersController = require ('../controllers/custumers')
+
 routes.get('/', (req,res)=>{
     res.render("index")
 })
+
+routes.get('/register', (req,res)=>{
+    res.render('register')    
+})
+
+routes.post('/register/add', CustumersController.add)
 
 module.exports= routes
